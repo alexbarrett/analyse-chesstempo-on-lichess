aee5a991fb4c2a8316a947e2f82788259805a7fd
Alex Barrett <alex@reason.co>
2020-09-09 23:28:40 +0100

    Fix compatibility with current version of ChessTempo

--- src/analyse-chesstempo-on-lichess.js
+++ src/analyse-chesstempo-on-lichess.js
@@ -1,4 +1,20 @@
 /**
+ * Enable this flag in development to see debug messages.
+ */
+const DEBUG = false;
+
+/**
+ * Print a debug message.
+ *
+ * It's the same as console.log.
+ */
+function debug(...args) {
+  if (DEBUG) {
+    console.log("[Analyse ChessTempo on Lichess]", ...args);
+  }
+}
+
+/**
  * @param {Element} target
  * @param {string} selector
  * @param {(element: Element) => void} listener
@@ -18,14 +34,15 @@ function onChildElementAdded(target, selector, listener) {
 
 /**
  * @param {Element} target
- * @param {(element: Element) => void} listener
+ * @param {() => void} listener
  */
-function onClassChanged(target, listener) {
-  new MutationObserver(mutations => {
+function onElementMutated(target, listener) {
+  new MutationObserver(() => {
     listener();
   }).observe(target, {
+    subtree: true,
     attributes: true,
-    attributeFilter: ["class"]
+    attributeFilter: ["class"],
   });
 }
 
@@ -40,10 +57,11 @@ function addLichessButton(boardControls, action) {
         title: "Analyse on Lichess",
         label: "<span class='analyse-chesstempo-on-lichess'/>",
         name: "lichess",
+        // 'after' option does not work due to a bug in ChessTempo.
         after: "analyse",
-        action
-      }
-    ]
+        action,
+      },
+    ],
   });
 }
 
@@ -57,30 +75,59 @@ function openFen(fen) {
 }
 
 /**
- * @param {Element} analyseButton Must be a child of a 'board-controls' element,
- *  which in turn must be a child of a 'chess-tactics' element.
+ * @param {HTMLElement} element
  */
-function init(analyseButton) {
-  const boardControls = analyseButton.closest("board-controls");
+function isElementVisible(element) {
+  return element.offsetParent !== null;
+}
+
+/**
+ * @param {Element} button
+ *   Must be a child of a 'board-controls' element, which in turn must be a
+ *   child of a 'chess-tactics' element.
+ */
+function init(button) {
+  const boardControls = button.closest("board-controls");
   const tactics = boardControls && boardControls.closest("chess-tactics");
-  if (tactics) {
-    addLichessButton(boardControls, () => {
-      openFen(tactics.probData.startPosition);
-    });
-    onClassChanged(analyseButton, () => {
-      if (analyseButton.classList.contains("ct-hidden")) {
-        boardControls.hideActions(["lichess"]);
-      } else {
-        boardControls.showActions(["lichess"]);
-      }
-    });
+  if (!tactics) {
+    debug(
+      `Could not find 'chess-tactics' element (board-controls = ${boardControls})`
+    );
+    return;
   }
+
+  let buttonVisible = false;
+  const handleBoardControlsMutation = () => {
+    if (isElementVisible(button) === buttonVisible) {
+      // Avoid setting the button to a state it's already in. Doing so causes an
+      // infinite loop between mutation <-> handler.
+      return;
+    }
+    if (isElementVisible(button)) {
+      debug("Showing Lichess button");
+      boardControls.showActions(["lichess"]);
+      buttonVisible = true;
+    } else {
+      debug("Hiding Lichess button");
+      boardControls.hideActions(["lichess"]);
+      buttonVisible = false;
+    }
+  };
+
+  addLichessButton(boardControls, () => {
+    openFen(tactics.probData.startPosition);
+  });
+  debug("Lichess button added to board-controls");
+  onElementMutated(boardControls, handleBoardControlsMutation);
 }
 
 const tactics = document.querySelector("chess-tactics");
-const analyseButton = tactics.querySelector(".ct-board-action-analyse");
+const analyseButton =
+  tactics && tactics.querySelector(".ct-board-action-analyse");
 if (analyseButton) {
+  debug("Found analyse button", analyseButton);
   init(analyseButton);
 } else {
+  debug("Waiting for analyse button");
   onChildElementAdded(tactics, ".ct-board-action-analyse", init);
 }
